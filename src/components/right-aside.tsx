a6b0efcaa7b68021c62a588780fdff65edd9d679
Jazim Abbas <jazimabbas87@gmail.com>
2024-02-29 16:48:56 +0500

    Right Aside: Added Projects section

--- src/components/right-aside.tsx
+++ src/components/right-aside.tsx
@@ -11,11 +11,28 @@ export function RightAside() {
           <span>S</span>
         </CommandWrapper>
       </SearchWrapper>
+      <ProjectsWrapper>
+        <Heading>Projects</Heading>
+        <Divider />
+        <Items>
+          <li>
+            <a href="">Codewell</a>
+          </li>
+          <li>
+            <a href="">Spense</a>
+          </li>
+          <li>
+            <a href="">YelCamp</a>
+          </li>
+        </Items>
+      </ProjectsWrapper>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.aside`
+  --padding: 15px;
+
   flex-basis: 210px;
   display: flex;
   flex-direction: column;
@@ -27,7 +44,7 @@ const SearchWrapper = styled.div`
   justify-content: space-between;
   gap: 5px;
   border: 1px solid var(--color-border);
-  padding: 10px;
+  padding: var(--padding);
   font-size: 14px;
   border-radius: 5px;
 `;
@@ -45,4 +62,41 @@ const CommandWrapper = styled.div`
   border: 1px solid var(--color-border);
   align-items: center;
   padding: 4px;
+`;
+
+const ProjectsWrapper = styled.div`
+  --gap: 20px;
+
+  padding: calc(var(--padding) * 2) var(--padding);
+  border: 1px solid var(--color-border);
+  display: flex;
+  flex-direction: column;
+  gap: var(--gap);
+`;
+
+const Divider = styled.div`
+  height: 1px;
+  width: 100%;
+  background-color: var(--color-border);
+`;
+
+const Heading = styled.h3`
+  color: #110e27;
+  font-weight: 700;
+`;
+
+const Items = styled.ul`
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  gap: var(--gap);
+
+  li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
 `;
